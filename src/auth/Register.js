be1b164f86db7f9129cb7bb4f46b02fa1cc48b8c
CarlosGRJ <carlosgrj@outlook.com>
2021-03-27 19:24:38 -0600

    Register

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import { RegisterForm } from '../components/RegisterForm';
 
 export const Register = () => {
-    return (
-        <div className='container-fluid h1 p-5 text-center'>Register Page</div>
-    )
-}
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      //   console.table({ name, email, password });
+      try {
+         const res = await axios.post('http://localhost:8000/api/register', {
+            name,
+            email,
+            password,
+         });
+         console.log('REGISTER USER ====> ', res);
+      } catch (error) {
+         console.log(error);
+      }
+   };
+
+   return (
+      <>
+         <div className='container-fluid bg-secondary p-5 text-center'>
+            <h1>Register</h1>
+         </div>
+
+         <div className='container'>
+            <div className='row'>
+               <div className='col-md-6 offset-md-3'>
+                  <RegisterForm
+                     handleSubmit={handleSubmit}
+                     name={name}
+                     setName={setName}
+                     email={email}
+                     setEmail={setEmail}
+                     password={password}
+                     setPassword={setPassword}
+                  />
+               </div>
+            </div>
+         </div>
+      </>
+   );
+};
