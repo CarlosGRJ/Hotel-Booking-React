decf3c2d4e531e05cfb5205e3437f53e82e86561
CarlosGRJ <carlosgrj@outlook.com>
2021-04-02 17:48:38 -0600

    Display Hotel Features

--- src/actions/stripe.js
+++ src/actions/stripe.js
@@ -34,7 +34,7 @@ export const getAccountBalance = async (token) =>
    );
 
 export const currencyFormatter = (data) => {
-   return (data.amount / 100).toLocaleString(data.currency, {
+   return data.amount.toLocaleString(data.currency, {
       style: 'currency',
       currency: data.currency,
    });
---
+++ src/components/cards/SmallCard.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { diffDays } from '../../actions/hotel';
+import { currencyFormatter } from '../../actions/stripe';
+import { Link } from 'react-router-dom';
+
+export const SmallCard = ({ h, handleHotelDelete = (f) => f }) => {
+   const history = useHistory();
+
+   return (
+      <>
+         <div className='card mb-3'>
+            <div className='row no-gutters'>
+               <div className='col-md-4'>
+                  <img
+                     className='card-image img img-fluid'
+                     src='https://via.placeholder.com/900x500.png?text=MERN+Booking'
+                     alt='default_hotel_image'
+                  />
+               </div>
+               <div className='col-md-8'>
+                  <div className='card-body'>
+                     <h3 className='card-title'>
+                        {h.title}{' '}
+                        <span className='float-right text-primary'>
+                           {currencyFormatter({
+                              amount: h.price,
+                              currency: 'usd',
+                           })}
+                        </span>
+                     </h3>
+                     <p className='alert alert-info'>{h.location}</p>
+                     <p className='card-text'>{`${h.content.substring(
+                        1,
+                        200,
+                     )}...`}</p>
+                     <p className='card-text'>
+                        <span className='float-right text-primary'>
+                           for {diffDays(h.from, h.to)}{' '}
+                           {diffDays(h.from, h.to) <= 1 ? 'day' : 'days'}
+                        </span>
+                     </p>
+                     <p className='card-text'>{h.bed} bed</p>
+                     <p className='card-text'>
+                        Available from {new Date(h.from).toLocaleDateString()}
+                     </p>
+
+                     <div className='d-flex justify-content-between h4'>
+                        <button
+                           onClick={() => history.push(`/hotel/${h._id}`)}
+                           className='btn btn-primary'>
+                           Show more
+                        </button>
+                        <Link to={`/hotel/edit/${h._id}`}>
+                           <EditOutlined className='text-warning' />
+                        </Link>
+                        <DeleteOutlined
+                           onClick={() => handleHotelDelete(h._id)}
+                           className='text-danger'
+                        />
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </>
+   );
+};
